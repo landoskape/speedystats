45984213b305e24448821a4833455bf574e2b8e5
Andrew Landau <andrewtylerlandau@gmail.com>
2025-01-08 12:41:10 +0000

    Add codebase

---
+++ faststats/__init__.py
@@ -0,0 +1,23 @@
+from .faststats import (
+    sum,
+    nansum,
+    ptp,
+    percentile,
+    nanpercentile,
+    quantile,
+    nanquantile,
+    median,
+    average,
+    mean,
+    std,
+    var,
+    nanmedian,
+    nanmean,
+    nanstd,
+    nanvar,
+    zscore,
+    median_zscore,
+    nanmedian_zscore,
+)
+
+__version__ = "0.0.0"
