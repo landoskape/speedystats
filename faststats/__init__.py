899fcfac24cc2e8b760bb71386aed1f0bf2a9a60
landoskape <andrewtylerlandau@gmail.com>
2025-01-30 21:38:41 +0000

    top level faststats code

--- faststats/__init__.py
+++ faststats/__init__.py
@@ -1,22 +1,18 @@
-from .faststats import (
-    sum,
-    nansum,
-    ptp,
-    percentile,
-    nanpercentile,
-    quantile,
-    nanquantile,
-    median,
-    average,
-    mean,
-    std,
-    var,
-    nanmedian,
-    nanmean,
-    nanstd,
-    nanvar,
-    zscore,
-    nan_zscore,
-)
+"""Numba-accelerated statistical functions."""
 
-__version__ = "0.0.0"
+from .faststats import sum
+from .faststats import nansum
+from .faststats import ptp
+from .faststats import percentile
+from .faststats import nanpercentile
+from .faststats import quantile
+from .faststats import nanquantile
+from .faststats import median
+from .faststats import nanmedian
+from .faststats import average
+from .faststats import mean
+from .faststats import nanmean
+from .faststats import std
+from .faststats import nanstd
+from .faststats import var
+from .faststats import nanvar
